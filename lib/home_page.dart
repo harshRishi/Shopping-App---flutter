cff3ce6cbf406176a438648072e21d7f58f48a1d
Harsh <harshuniverse000@gmail.com>
2024-04-04 18:16:41 +0530

    Header completed inside home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,93 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Adidas', 'Nike', 'Puma'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.elliptical(20, 20)),
     );
+
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          // Heading
+          const Row(children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Shoes\nCollection",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
+                ),
+              ),
+            )
+          ]),
+
+          // Filters
+          SizedBox(
+            height: 80,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter;
+                      });
+                    },
+                    child: Chip(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8.0),
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context).colorScheme.primary // gettting it from seed's primary color using inheritance
+                            : const Color.fromRGBO(245, 247, 249, 1),
+                        side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        label: Text(
+                          filter,
+                          style: const TextStyle(fontSize: 16),
+                        )),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Lato',
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromRGBO(254, 206, 1, 1)),
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+          primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
       ),
       home: const HomePage(),
     );
