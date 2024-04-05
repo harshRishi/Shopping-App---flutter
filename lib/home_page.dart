18a05b7758809c27d325ee56dcfa85c59771d71d
Harsh <harshuniverse000@gmail.com>
2024-04-05 12:54:50 +0530

    Separated ProductList page + bottom Navigation Bar is added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_flutter/global_varaible.dart';
-import 'package:shop_app_flutter/product_cards.dart';
-import 'package:shop_app_flutter/product_details_page.dart';
+import 'package:shop_app_flutter/cart_page.dart';
+import 'package:shop_app_flutter/products_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,118 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> filters = const ['All', 'Adidas', 'Nike', 'Puma'];
-  late String selectedFilter;
-
-  @override
-  void initState() {
-    super.initState();
-    selectedFilter = filters[0];
-  }
+  int currentPage = 0;
+  List<Widget> pages = const [ProductsList(), CartPage()];
 
   @override
   Widget build(BuildContext context) {
-    const border = OutlineInputBorder(
-      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
-      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
-    );
-
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          // Heading
-          Row(children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Shoes\nCollection",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            const Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  prefixIcon: Icon(Icons.search),
-                  border: border,
-                  enabledBorder: border,
-                  focusedBorder: border,
-                ),
-              ),
-            )
-          ]),
-
-          // Filters
-          SizedBox(
-            height: 80,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: filters.length,
-              itemBuilder: (context, index) {
-                final filter = filters[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedFilter = filter;
-                      });
-                    },
-                    child: Chip(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8.0),
-                        backgroundColor: selectedFilter == filter
-                            ? Theme.of(context)
-                                .colorScheme
-                                .primary // gettting it from seed's primary color using inheritance
-                            : const Color.fromRGBO(245, 247, 249, 1),
-                        side: const BorderSide(
-                          color: Color.fromRGBO(245, 247, 249, 1),
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        label: Text(
-                          filter,
-                          style: const TextStyle(fontSize: 16),
-                        )),
-                  ),
-                );
-              },
-            ),
-          ),
-
-          // Products
-          Expanded(
-            child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  final product = products[index];
-                  return GestureDetector(
-                    onTap: () {
-                      // Works similar to router in next
-                      Navigator.of(context).push(
-                        // Here MaterialPageRoute is use for android transition from one route to another
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return ProductDetailsPage(product: products[index]);
-                          },
-                        ),
-                      );
-                    },
-                    child: ProductCard(
-                      title: product['title'] as String,
-                      price: product['price'] as double,
-                      image: product['imageUrl'] as String,
-                      backgroundColor: index.isEven
-                          ? const Color.fromRGBO(216, 240, 253, 1)
-                          : const Color.fromRGBO(245, 247, 249, 1),
-                    ),
-                  );
-                }),
-          )
+      // IndexedStack maintain the scroll when page is change and also page the page as index chnages
+      // Mean while we can also use body: pages[currentPage] but this won't maintain the scroll here
+      body: IndexedStack(index: currentPage, children: pages),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        iconSize: 35,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
         ],
       ),
-    ));
+    );
   }
 }
---
+++ lib/products_list.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/global_varaible.dart';
+import 'package:shop_app_flutter/product_cards.dart';
+import 'package:shop_app_flutter/product_details_page.dart';
+
+class ProductsList extends StatefulWidget {
+  const ProductsList({super.key});
+
+  @override
+  State<ProductsList> createState() => _ProductsListState();
+}
+
+class _ProductsListState extends State<ProductsList> {
+  final List<String> filters = const ['All', 'Adidas', 'Nike', 'Puma'];
+  late String selectedFilter;
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+    );
+
+    return SafeArea(
+      child: Column(
+        children: [
+          // Heading
+          Row(children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Shoes\nCollection",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
+                ),
+              ),
+            )
+          ]),
+
+          // Filters
+          SizedBox(
+            height: 80,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter;
+                      });
+                    },
+                    child: Chip(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8.0),
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context)
+                                .colorScheme
+                                .primary // gettting it from seed's primary color using inheritance
+                            : const Color.fromRGBO(245, 247, 249, 1),
+                        side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        label: Text(
+                          filter,
+                          style: const TextStyle(fontSize: 16),
+                        )),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // Products
+          Expanded(
+            child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return GestureDetector(
+                    onTap: () {
+                      // Works similar to router in next
+                      Navigator.of(context).push(
+                        // Here MaterialPageRoute is use for android transition from one route to another
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ProductDetailsPage(product: products[index]);
+                          },
+                        ),
+                      );
+                    },
+                    child: ProductCard(
+                      title: product['title'] as String,
+                      price: product['price'] as double,
+                      image: product['imageUrl'] as String,
+                      backgroundColor: index.isEven
+                          ? const Color.fromRGBO(216, 240, 253, 1)
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
